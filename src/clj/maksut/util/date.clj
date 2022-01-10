9247774e31e42a323e89009ee0a1784f094e0327
Simo Tuokko <simo.tuokko@deftit.com>
2022-01-10 02:18:57 +0200

    Use due-date field when creating new invoice HLE-1495 - missing date.clj

---
+++ src/clj/maksut/util/date.clj
@@ -0,0 +1,17 @@
+(ns maksut.util.date
+  (:require [clj-time.core :as time]
+            [clj-time.format :as format]
+            [clojure.string :as string]))
+
+(defonce date-formatter (format/formatters :date))
+(defonce fi-formatter (format/formatter "dd.MM.yyyy"))
+
+(defn iso-date-str->date [date-str]
+  (when-not (string/blank? date-str)
+            (try
+              (format/parse-local-date date-formatter date-str)
+              (catch Exception _))))
+
+(defn format-date [iso-date-str]
+  (when-let [date (iso-date-str->date iso-date-str)]
+    (format/unparse fi-formatter date)))
