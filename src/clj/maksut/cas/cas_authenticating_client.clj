e5f24bda1310e7943a80102bf2fe9b4545edf289
Jussi Jartamo <jussi.jartamo@gofore.com>
2022-03-02 13:25:16 +0200

    -

--- src/clj/maksut/cas/cas_authenticating_client.clj
+++ src/clj/maksut/cas/cas_authenticating_client.clj
@@ -58,7 +58,7 @@
       (do-cas-authenticated-request {:cas-client          (:cas-client this)
                                      :method              :post
                                      :url                 url
-                                     :content-type        "application/json;charset=utf8"
+                                     :content-type        "application/json;charset=utf-8"
                                      :body                body}
                                     config))
 
@@ -69,7 +69,7 @@
     (do-cas-authenticated-request {:cas-client          (:cas-client this)
                                    :method              :put
                                    :url                 url
-                                   :content-type        "application/json;charset=utf8"
+                                   :content-type        "application/json;charset=utf-8"
                                    :body                body}
                                   config))
 
