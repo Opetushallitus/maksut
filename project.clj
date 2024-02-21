35812bafa161ede9b8e49973ffce6a1e1cbd5f40
Väinö Ala-Härkönen <vae@wonna.fi>
2024-02-21 11:36:23 +0200

    OY-4695: easy version updates, manual and unit tests pass without changes

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
 (defproject maksut "0.1.0-SNAPSHOT"
   ;TODO Tässä on todennäköisesti paljon kopioitua depsejä mitä ei tarvita, käy läpi myöhemmin mitkä voi jättää pois
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/clojurescript "1.10.773"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library
                                org.clojure/google-closure-library-third-party]]
-                 [camel-snake-kebab "0.4.1"]
-                 [cheshire "5.10.0"]
-                 [clj-http "3.10.3"]
+                 [camel-snake-kebab "0.4.3"]
+                 [cheshire "5.12.0"]
+                 [clj-http "3.12.3"]
                  [clj-time "0.15.2"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.12.396"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.12.663"]
                  [com.andrewmcveigh/cljs-time "0.5.2"]
                  [com.taoensso/timbre "4.10.0"]
                  [com.fzakaria/slf4j-timbre "0.3.20"]
                  [timbre-ns-pattern-level "0.1.2"]
-                 [com.stuartsierra/component "1.0.0"]
+                 [com.stuartsierra/component "1.1.0"]
                  [clj-soup/clojure-soup "0.1.3"]
                  [org.flywaydb/flyway-core "7.0.2"]
                  [fi.vm.sade/auditlogger "9.0.0-SNAPSHOT"]
@@ -25,29 +25,29 @@
                  [oph/clj-stdout-access-logging "1.0.0-SNAPSHOT"]
                  [oph/clj-timbre-access-logging "1.0.0-SNAPSHOT"]
                  [oph/clj-ring-db-cas-session "0.3.0-SNAPSHOT"]
-                 [hikari-cp "2.13.0"]
-                 [metosin/reitit "0.5.12"]
-                 [metosin/schema-tools "0.12.2"]
-                 [org.clojure/core.async "1.3.610"]
-                 [org.clojure/java.jdbc "0.7.11"]
-                 [org.clojure/core.match "1.0.0"]
-                 [org.postgresql/postgresql "42.2.17"]
-                 [com.layerware/hugsql "0.5.1"]
+                 [hikari-cp "3.0.1"]
+                 [metosin/reitit "0.6.0"]
+                 [metosin/schema-tools "0.13.1"]
+                 [org.clojure/core.async "1.6.681"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.clojure/core.match "1.1.0"]
+                 [org.postgresql/postgresql "42.7.2"]
+                 [com.layerware/hugsql "0.5.3"]
                  [re-frame "1.2.0"]
                  [reagent "1.0.0"]
-                 [markdown-clj "1.10.4"]
-                 [com.googlecode.owasp-java-html-sanitizer/owasp-java-html-sanitizer "20191001.1" :exclusions [com.google.guava/guava]]
-                 [com.fasterxml.jackson.core/jackson-core "2.12.1"]
-                 [com.fasterxml.jackson.core/jackson-databind "2.12.1"]
-                 [ring/ring-defaults "0.3.2"]
-                 [ring/ring-json "0.5.0"]
-                 [ring/ring-session-timeout "0.2.0"]
-                 [selmer "1.12.31"]
+                 [markdown-clj "1.11.9"]
+                 [com.googlecode.owasp-java-html-sanitizer/owasp-java-html-sanitizer "20220608.1" :exclusions [com.google.guava/guava]]
+                 [com.fasterxml.jackson.core/jackson-core "2.15.2"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.15.2"]
+                 [ring/ring-defaults "0.4.0"]
+                 [ring/ring-json "0.5.1"]
+                 [ring/ring-session-timeout "0.3.0"]
+                 [selmer "1.12.59"]
                  [stylefy "2.2.1"
                   :exclusions [[org.clojure/core.async]]]
-                 [prismatic/schema "1.1.12"]
+                 [prismatic/schema "1.4.1"]
                  [thheller/shadow-cljs "2.11.23"]
-                 [yogthos/config "1.1.7"]
+                 [yogthos/config "1.2.0"]
                  [environ "1.2.0"]
                  [oph/clj-timbre-auditlog "0.1.0-SNAPSHOT"]]
 
@@ -88,18 +88,18 @@
 
   :profiles
   {:dev
-            {:dependencies [[binaryage/devtools "1.0.2"]
-                            [clj-kondo "2020.10.10"]
+            {:dependencies [[binaryage/devtools "1.0.7"]
+                            [clj-kondo "2024.02.12"]
                             [day8.re-frame/re-frame-10x "1.1.13"]
                             [day8.re-frame/tracing "0.6.2"]
                             [reloaded.repl "0.2.4"]
-                            [clj-http-fake "1.0.3"]]
+                            [clj-http-fake "1.0.4"]]
              :source-paths ["dev/clj" "dev/cljs"]}
 
    :prod    {:dependencies [[day8.re-frame/tracing-stubs "0.6.2"]]}
 
    :uberjar {:source-paths ["env/prod/clj"]
-             :dependencies [[day8.re-frame/tracing-stubs "0.6.0"]]
+             :dependencies [[day8.re-frame/tracing-stubs "0.6.2"]]
              :omit-source  false
              :aot          [maksut.core]
              :uberjar-name "maksut.jar"
